4391291488cbcef7aa11906a3eda3cf090c65688
Alen Valdez <alenkvaldeztch@gmail.com>
2023-03-12 15:09:34 -0700

    weeklyset 4 completed

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -7,6 +7,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
+<<<<<<< Updated upstream
             for (int i = 0; i < vals.Length; i++)
             {
                 if (vals[i])
@@ -17,11 +18,24 @@ namespace ChallengesWithTestsMark8
                 {
                     return false;
                 }
+=======
+           for (int i = 0; i < vals.Length; i++)
+            {
+                if (vals[i])
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+>>>>>>> Stashed changes
             }
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
+<<<<<<< Updated upstream
             if(numbers == null || numbers.Count() == 0)
             {
                 return false;
@@ -29,11 +43,21 @@ namespace ChallengesWithTestsMark8
 
             var sum = numbers.Sum();
             return (sum % 2 != 0);  
+=======
+            if(numbers == null || numbers.Count() == 0)
+            {
+                return false;
+            }
+
+            var sum = numbers.Sum();
+            return (sum % 2 != 0);
+>>>>>>> Stashed changes
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
             var isLower = false;
+<<<<<<< Updated upstream
             var isUpper = false;    
             var isNumber = false;
 
@@ -48,11 +72,48 @@ namespace ChallengesWithTestsMark8
                     return false;
                 }
             }
+=======
+            var isUpper = false;
+            var isNumber = false;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLower(password[i]))
+                {
+                    isLower = true; 
+                }
+
+                if (char.IsUpper(password[i]))
+                {
+                    isUpper = true;
+                }
+
+                if (char.IsNumber(password[i]))
+                {
+                    isNumber = true;
+                }
+
+                if(isLower && isUpper && isNumber)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+         
+>>>>>>> Stashed changes
         }
 
         public char GetFirstLetterOfString(string val)
         {
+<<<<<<< Updated upstream
             return val.First();
+=======
+            return val[0];
+>>>>>>> Stashed changes
         }
 
         public char GetLastLetterOfString(string val)
@@ -62,10 +123,18 @@ namespace ChallengesWithTestsMark8
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
+<<<<<<< Updated upstream
             if (divisor == 0)
             {
                 return 0;
             }
+=======
+            if(divisor == 0)
+            {
+                return 0;
+            }
+
+>>>>>>> Stashed changes
             return dividend / divisor;
         }
 
@@ -77,6 +146,7 @@ namespace ChallengesWithTestsMark8
         public int[] GetOddsBelow100()
         {
             var list = new List<int>();
+<<<<<<< Updated upstream
 
            for (int i =0; i < 100; i++)
             {
@@ -87,13 +157,30 @@ namespace ChallengesWithTestsMark8
             }
 
            return list.ToArray();
+=======
+            for (int i = 0; i < 100; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    list.Add(i);
+                }
+            }
+
+            return list.ToArray();
+>>>>>>> Stashed changes
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
+<<<<<<< Updated upstream
            for (int i = 0; i < words.Length; i++)
             {
                 words[i] = words[i].ToUpper();
+=======
+           for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+>>>>>>> Stashed changes
             }
         }
     }
